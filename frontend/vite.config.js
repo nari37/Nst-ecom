5fefe2934d4b584667e5d92cd1e897feeed68aca
nari37 <nareshpogiri409@gmail.com>
2025-01-31 12:36:57 +0530

    "change back the ports"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 5173, // Use the PORT variable provided by Render
-  },
-});
+  plugins: [react()],
+  server: {port: 5173}
+})
